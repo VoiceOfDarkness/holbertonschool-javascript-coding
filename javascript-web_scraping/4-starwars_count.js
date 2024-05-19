6b60abfc43aab8643aaa1c341ff2e96826fc6237
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-19 13:24:53 +0300

    5th task

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const { count } = require('console');
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((film) => {
+    const filteredCharacters = film.characters.filter((character) =>
+      character.includes('18')
+    );
+    if (filteredCharacters.length > 0) {
+      count++;
+    }
+  });
+  console.log(count);
+});
