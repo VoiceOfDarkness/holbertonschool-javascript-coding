bdc2b0d9084c0147dc66497284d7b92194c37911
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-19 12:49:57 +0300

    hot fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,11 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(filePath, string, 'utf-8', (err, data) => {
+fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-
-  console.log(data);
 });
