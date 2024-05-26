9e3461f74d1d2ec0d5893b7cd91f26013aff7443
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-26 19:58:34 +0300

    2nd task

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+process.stdin.setEncoding("utf8");
+
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.on("data", (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
