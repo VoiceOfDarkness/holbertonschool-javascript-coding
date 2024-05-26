ad6120c97cd211674a6d4086ced9d4d76316a777
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-26 19:59:40 +0300

    hot fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
-process.stdin.setEncoding("utf8");
-
 console.log("Welcome to Holberton School, what is your name?");
-
-process.stdin.on("data", (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-});
-
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.resume();
+process.stdin.on("readable", () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write("This important software is now closing\n");
+    process.exit();
+  }
 });
